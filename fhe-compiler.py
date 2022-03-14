1ec48a70143f3c557f1a1fb8f6d088a18054117b
Alexander Viand <alexander.viand@inf.ethz.ch>
2022-03-14 14:45:24 +0000

    hardcode in test for now

--- fhe-compiler.py
+++ fhe-compiler.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from xdsl import *
 from xdsl.ir import *
 from xdsl.irdl import *
@@ -24,3 +25,9 @@ poly = Poly(context)
 
 # Printer used to pretty-print MLIR data structures
 printer = Printer()
+
+# Simple hardcoded test, doesn't work for now because we didn't define custom assembly
+test_program = Path('tests/multiply.mlir').read_text()
+parser = Parser(context, test_program)
+module = parser.parse_op()
+module.verify()
